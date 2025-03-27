027fb8a2293764dd5d4d2e1b9752dcdf8caa68af
migue <migue@ZENBOOK-MIGUE>
2025-03-26 21:16:38 -0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NLP_Prototype_MVC_8._0.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NLP_Prototype_MVC_8._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
